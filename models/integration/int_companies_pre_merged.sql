00c7e4b8d93f8ca0460ad3d9b3f9ea48c213627b
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-05-04 20:33:30 -0600

    chore: apply sqlfmt 0.8.0

--- models/integration/int_companies_pre_merged.sql
+++ models/integration/int_companies_pre_merged.sql
@@ -1,104 +1,123 @@
 
-{% if var('crm_warehouse_company_sources') %}
+{% if var("crm_warehouse_company_sources") %}
 
-{{config(materialized="table")}}
+{{ config(materialized="table") }}
 
-with t_companies_pre_merged as (
+with
+    t_companies_pre_merged as (
 
-    {% for source in var('crm_warehouse_company_sources') %}
-      {% set relation_source = 'stg_' + source + '_companies' %}
+        {% for source in var("crm_warehouse_company_sources") %}
+        {% set relation_source = "stg_" + source + "_companies" %}
 
-      select
-        '{{source}}' as source,
-        *
+        select '{{source}}' as source, *
         from {{ ref(relation_source) }}
 
-        {% if not loop.last %}union all{% endif %}
-      {% endfor %}
+        {% if not loop.last %} union all{% endif %}
+        {% endfor %}
 
     ),
 
-{% if target.type == 'bigquery' %}
+    {% if target.type == "bigquery" %}
 
-      all_company_ids as (
-             SELECT company_name, array_agg(distinct company_id ignore nulls) as all_company_ids
-             FROM t_companies_pre_merged
-             group by 1),
-      all_company_addresses as (
-             SELECT company_name, array_agg(struct(company_address,
-                                                   company_address2,
-                                                   company_city,
-                                                   company_state,
-                                                   company_country,
-                                                   company_zip) ignore nulls) as all_company_addresses
-             FROM t_companies_pre_merged
-             group by 1),
+    all_company_ids as (
+        select
+            company_name, array_agg(distinct company_id ignore nulls) as all_company_ids
+        from t_companies_pre_merged
+        group by 1
+    ),
+    all_company_addresses as (
+        select
+            company_name,
+            array_agg(
+                struct(
+                    company_address,
+                    company_address2,
+                    company_city,
+                    company_state,
+                    company_country,
+                    company_zip
+                ) ignore nulls
+            ) as all_company_addresses
+        from t_companies_pre_merged
+        group by 1
+    ),
 
-{% elif target.type == 'snowflake' %}
+    {% elif target.type == "snowflake" %}
 
-      all_company_ids as (
-          SELECT company_name,
-                 array_agg(
-                    distinct company_id
-                  ) as all_company_ids
-            FROM t_companies_pre_merged
-          group by 1),
-      all_company_addresses as (
-          SELECT company_name,
-                 array_agg(
-                      parse_json (
-                        concat('{"company_address":"',company_address,
-                               '", "company_address2":"',company_address2,
-                               '", "company_city":"',company_city,
-                               '", "company_state":"',company_state,
-                               '", "company_country":"',company_country,
-                               '", "company_zip":"',company_zip,'"} ')
-                      )
-                 ) as all_company_addresses
-          FROM t_companies_pre_merged
-          where length(coalesce(company_address,company_address2,company_city,company_state,company_country,company_zip)) >0
-          group by 1
-      ),
+    all_company_ids as (
+        select company_name, array_agg(distinct company_id) as all_company_ids
+        from t_companies_pre_merged
+        group by 1
+    ),
+    all_company_addresses as (
+        select
+            company_name,
+            array_agg(
+                parse_json(
+                    concat(
+                        '{"company_address":"',
+                        company_address,
+                        '", "company_address2":"',
+                        company_address2,
+                        '", "company_city":"',
+                        company_city,
+                        '", "company_state":"',
+                        company_state,
+                        '", "company_country":"',
+                        company_country,
+                        '", "company_zip":"',
+                        company_zip,
+                        '"} '
+                    )
+                )
+            ) as all_company_addresses
+        from t_companies_pre_merged
+        where
+            length(
+                coalesce(
+                    company_address,
+                    company_address2,
+                    company_city,
+                    company_state,
+                    company_country,
+                    company_zip
+                )
+            ) > 0
+        group by 1
+    ),
 
-{% else %}
+    {% else %}
     {{ exceptions.raise_compiler_error(target.type ~" not supported in this project") }}
 
-{% endif %}
+    {% endif %}
 
-grouped as (
-      SELECT
-      company_name,
-      max(company_phone) as company_phone,
-      max(company_website) as company_website,
-      max(company_industry) as company_industry,
-      max(company_linkedin_company_page) as company_linkedin_company_page,
-      max(company_linkedin_bio) as company_linkedin_bio,
-      max(company_twitterhandle) as company_twitterhandle,
-      max(company_description) as company_description,
-      max(company_finance_status) as company_finance_status,
-      max(company_currency_code) as company_currency_code,
-      min(company_created_date) as company_created_date,
-      max(company_last_modified_date) as company_last_modified_date
-    from t_companies_pre_merged
-      group by 1
-),
-joined as (
-      SELECT i.all_company_ids,
-      g.*,
-      a.all_company_addresses
-      FROM grouped g
-      JOIN all_company_ids i ON g.company_name = i.company_name
-      LEFT OUTER JOIN all_company_addresses a ON g.company_name = a.company_name
-)
-select * from joined
-
-{% else %}
-
-{{
-    config(
-        enabled=false
+    grouped as (
+        select
+            company_name,
+            max(company_phone) as company_phone,
+            max(company_website) as company_website,
+            max(company_industry) as company_industry,
+            max(company_linkedin_company_page) as company_linkedin_company_page,
+            max(company_linkedin_bio) as company_linkedin_bio,
+            max(company_twitterhandle) as company_twitterhandle,
+            max(company_description) as company_description,
+            max(company_finance_status) as company_finance_status,
+            max(company_currency_code) as company_currency_code,
+            min(company_created_date) as company_created_date,
+            max(company_last_modified_date) as company_last_modified_date
+        from t_companies_pre_merged
+        group by 1
+    ),
+    joined as (
+        select i.all_company_ids, g.*, a.all_company_addresses
+        from grouped g
+        join all_company_ids i on g.company_name = i.company_name
+        left outer join all_company_addresses a on g.company_name = a.company_name
     )
-}}
+select *
+from joined
+
+{% else %} {{ config(enabled=false) }}
 
 
 {% endif %}
--- models/integration/int_contact_deals.sql
+++ models/integration/int_contact_deals.sql
@@ -1,30 +1,32 @@
-{% if target.type == 'bigquery' %}
+{% if target.type == "bigquery" %}
 {% if var("crm_warehouse_contact_sources") %}
-{% if 'hubspot_crm' in var("crm_warehouse_contact_sources") %}
-{% if 'hubspot_crm' in var("marketing_warehouse_deal_sources") %}
-{% if var("stg_hubspot_crm_etl") == 'stitch' %}
+{% if "hubspot_crm" in var("crm_warehouse_contact_sources") %}
+{% if "hubspot_crm" in var("marketing_warehouse_deal_sources") %}
+{% if var("stg_hubspot_crm_etl") == "stitch" %}
 
-WITH t_contact_deals_list AS (
+with
+    t_contact_deals_list as (
 
-  {% for source in var('marketing_warehouse_deal_sources') %}
-    {% set relation_source = 'stg_' + source + '_contact_deals' %}
+        {% for source in var("marketing_warehouse_deal_sources") %}
+        {% set relation_source = "stg_" + source + "_contact_deals" %}
 
-    select
-      '{{source}}' as source,
-      *
-      from {{ ref(relation_source) }}
+        select '{{source}}' as source, *
+        from {{ ref(relation_source) }}
 
-      {% if not loop.last %}union all{% endif %}
-    {% endfor %}
-)
-SELECT
-  *
-FROM
-  t_contact_deals_list
+        {% if not loop.last %} union all{% endif %}
+        {% endfor %}
+    )
+select *
+from t_contact_deals_list
 
 
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
