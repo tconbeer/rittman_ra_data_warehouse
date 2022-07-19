817237b7f3048e7ec056ffa1ca90be303d84acdb
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-07-19 11:56:37 -0600

    chore: apply sqlfmt fb99833

--- models/sources/stg_google_ads/models/google_ads_connector/stg_google_ads__ad_group_history.sql
+++ models/sources/stg_google_ads/models/google_ads_connector/stg_google_ads__ad_group_history.sql
@@ -1,55 +1,57 @@
-{% if target.type == 'bigquery' or target.type == 'snowflake' or target.type == 'redshift' %}
+{% if target.type == "bigquery" or target.type == "snowflake" or target.type == "redshift" %}
 {% if var("marketing_warehouse_ad_sources") %}
-{% if 'google_ads' in var("marketing_warehouse_ad_sources") %}
-{% if var("google_ads_api_source") == 'google_ads' %}
+{% if "google_ads" in var("marketing_warehouse_ad_sources") %}
+{% if var("google_ads_api_source") == "google_ads" %}
+with
+    base as (select * from {{ ref("stg_google_ads__ad_group_history_tmp") }}),
 
+    fields as (
 
-with base as (
+        select
+            {{
+                fivetran_utils.fill_staging_columns(
+                    source_columns=adapter.get_columns_in_relation(
+                        ref("stg_google_ads__ad_group_history_tmp")
+                    ),
+                    staging_columns=get_google_ads_google_ads_ad_group_history_columns(),
+                )
+            }}
 
-    select *
-    from {{ ref('stg_google_ads__ad_group_history_tmp') }}
+        from base
+    ),
 
-),
+    final as (
 
-fields as (
+        select
+            id as ad_group_id,
+            updated_at as updated_timestamp,
+            _fivetran_synced,
+            ad_group_type,
+            campaign_id,
+            campaign_name,
+            name as ad_group_name,
+            status as ad_group_status
+        from fields
+    ),
 
-    select
-        {{
-            fivetran_utils.fill_staging_columns(
-                source_columns=adapter.get_columns_in_relation(ref('stg_google_ads__ad_group_history_tmp')),
-                staging_columns=get_google_ads_google_ads_ad_group_history_columns()
-            )
-        }}
+    most_recent as (
 
-    from base
-),
+        select
+            *,
+            row_number() over (partition by ad_group_id order by updated_timestamp desc)
+            = 1 as is_most_recent_record
+        from final
 
-final as (
+    )
 
-    select
-        id as ad_group_id,
-        updated_at as updated_timestamp,
-        _fivetran_synced,
-        ad_group_type,
-        campaign_id,
-        campaign_name,
-        name as ad_group_name,
-        status as ad_group_status
-    from fields
-),
+select *
+from most_recent
 
-most_recent as (
-
-    select
-        *,
-        row_number() over (partition by ad_group_id order by updated_timestamp desc) = 1 as is_most_recent_record
-    from final
-
-)
-
-select * from most_recent
-
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
--- models/sources/stg_mailchimp_email/bigquery/stitch/stg_mailchimp_email_sends.sql
+++ models/sources/stg_mailchimp_email/bigquery/stitch/stg_mailchimp_email_sends.sql
@@ -1,39 +1,42 @@
-{{config(enabled = target.type == 'bigquery')}}
+{{ config(enabled=target.type == "bigquery") }}
 {% if var("marketing_warehouse_email_event_sources") %}
-{% if 'mailchimp_email' in var("marketing_warehouse_email_event_sources") %}
+{% if "mailchimp_email" in var("marketing_warehouse_email_event_sources") %}
 
-WITH source AS (
-  SELECT *
-  FROM {{ source('stitch_mailchimp_email', 'campaigns') }}
-),
-renamed as (
-SELECT
-  concat('{{ var('stg_mailchimp_email_id-prefix') }}',id) AS send_id,
-  content_type AS campaign_content_type,
-  create_time AS campaign_created_at_ts,
-  emails_sent AS total_campaign_emails_sent,
-  long_archive_url AS campaign_archive_url,
-  concat('{{ var('stg_mailchimp_email_id-prefix') }}',recipients.list_id) AS list_id,
-  recipients.list_is_active AS campaign_list_is_active,
-  recipients.list_name AS list_name,
-  recipients.recipient_count AS total_recipient_count,
-  report_summary.click_rate AS click_rate_pct,
-  report_summary.clicks AS total_clicks,
-  report_summary.open_rate AS open_rate_pct,
-  report_summary.opens AS total_opens,
-  report_summary.subscriber_clicks AS total_subscriber_clicks,
-  report_summary.unique_opens AS total_unique_opens,
-  resendable AS campaign_is_resendable,
-  send_time AS campaign_sent_ts,
-  settings.subject_line AS campaign_subject_line,
-  settings.title AS campaign_title,
-  status AS campaign_status,
-  tracking.html_clicks AS campaign_tracking_html_clicks,
-  tracking.opens AS campaign_tracking_opens,
-  tracking.text_clicks AS campaign_tracking_text_clicks
-FROM
-  source)
-select * from renamed
+with
+    source as (select * from {{ source("stitch_mailchimp_email", "campaigns") }}),
+    renamed as (
+        select
+            concat('{{ var(' stg_mailchimp_email_id - prefix ') }}', id) as send_id,
+            content_type as campaign_content_type,
+            create_time as campaign_created_at_ts,
+            emails_sent as total_campaign_emails_sent,
+            long_archive_url as campaign_archive_url,
+            concat(
+                '{{ var(' stg_mailchimp_email_id - prefix ') }}', recipients.list_id
+            ) as list_id,
+            recipients.list_is_active as campaign_list_is_active,
+            recipients.list_name as list_name,
+            recipients.recipient_count as total_recipient_count,
+            report_summary.click_rate as click_rate_pct,
+            report_summary.clicks as total_clicks,
+            report_summary.open_rate as open_rate_pct,
+            report_summary.opens as total_opens,
+            report_summary.subscriber_clicks as total_subscriber_clicks,
+            report_summary.unique_opens as total_unique_opens,
+            resendable as campaign_is_resendable,
+            send_time as campaign_sent_ts,
+            settings.subject_line as campaign_subject_line,
+            settings.title as campaign_title,
+            status as campaign_status,
+            tracking.html_clicks as campaign_tracking_html_clicks,
+            tracking.opens as campaign_tracking_opens,
+            tracking.text_clicks as campaign_tracking_text_clicks
+        from source
+    )
+select *
+from renamed
 
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
