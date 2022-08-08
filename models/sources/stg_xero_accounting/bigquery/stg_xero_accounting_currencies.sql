90306b7b38f72a616016c64ffeb28e216bb4c3b6
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-08-08 13:38:20 -0600

    chore: apply sqlfmt 6d33371

--- models/sources/stg_xero_accounting/bigquery/stg_xero_accounting_currencies.sql
+++ models/sources/stg_xero_accounting/bigquery/stg_xero_accounting_currencies.sql
@@ -1,21 +1,26 @@
-{{config(enabled = target.type == 'bigquery')}}
+{{ config(enabled=target.type == "bigquery") }}
 {% if var("finance_warehouse_currencies_sources") %}
-{% if 'xero_accounting' in var("finance_warehouse_currencies_sources") %}
+{% if "xero_accounting" in var("finance_warehouse_currencies_sources") %}
 
-{% if var("stg_xero_accounting_etl") == 'fivetran' %}
+{% if var("stg_xero_accounting_etl") == "fivetran" %}
 
-with source as (
-  select *
-  from {{ source('fivetran_xero_accounting','currency') }}
+with
+    source as (select * from {{ source("fivetran_xero_accounting", "currency") }}),
+    renamed as (
+        select
+            concat(
+                '{{ var(' stg_xero_accounting_id - prefix ') }}', code
+            ) as currency_code,
+            description as currency_name,
+        from source
+    )
 
-),
-renamed as (
-select concat('{{ var('stg_xero_accounting_id-prefix') }}',code) as currency_code,
-       description as currency_name, from source)
-
-select * from renamed
+select *
+from renamed
 
 {% endif %}
 
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
