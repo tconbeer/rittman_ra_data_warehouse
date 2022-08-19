c074a7410fe810748beefab00622fab9ed2ac119
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-08-19 15:58:06 -0600

    chore: apply sqlfmt 3e0f900

--- models/warehouse/w_ecommerce/redshift/wh_ecommerce_products_dim.sql
+++ models/warehouse/w_ecommerce/redshift/wh_ecommerce_products_dim.sql
@@ -1,23 +1,11 @@
 {% if var("ecommerce_warehouse_product_sources") %}
 
-{{
-    config(
-        unique_key='product_pk',
-        alias='products_dim'
-    )
-}}
+{{ config(unique_key="product_pk", alias="products_dim") }}
 
 
-WITH products AS
-  (
-  SELECT *
-  FROM
-     {{ ref('int_products') }} o
-)
-select    {{ dbt_utils.surrogate_key(
-          ['product_id']
-        ) }} as product_pk,
-          *
-FROM      products
+with products as (select * from {{ ref("int_products") }} o)
+select {{ dbt_utils.surrogate_key(["product_id"]) }} as product_pk, *
+from products
 
-{% else %} {{config(enabled=false)}} {% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
