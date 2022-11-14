b29aa4cea8d62a335ad751521bd0a8e4083773fd
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-11-14 23:35:50 +0000

    chore: apply sqlfmt b792a79

--- models/integration/int_conversations.sql
+++ models/integration/int_conversations.sql
@@ -1,20 +1,19 @@
-{% if var('crm_warehouse_conversations_sources') %}
+{% if var("crm_warehouse_conversations_sources") %}
+with
+    conversations_merge_list as (
+        {% for source in var("crm_warehouse_conversations_sources") %}
+        {% set relation_source = "stg_" + source + "_conversations" %}
 
-
-
-with conversations_merge_list as
-  (
-    {% for source in var('crm_warehouse_conversations_sources') %}
-      {% set relation_source = 'stg_' + source + '_conversations' %}
-
-      select
-        '{{source}}' as source,
-        *
+        select '{{source}}' as source, *
         from {{ ref(relation_source) }}
 
-        {% if not loop.last %}union all{% endif %}
-      {% endfor %}
-  )
-select * from conversations_merge_list
+        {% if not loop.last %}
+        union all
+        {% endif %}
+        {% endfor %}
+    )
+select *
+from conversations_merge_list
 
-{% else %} {{config(enabled=false)}} {% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
--- models/sources/stg_hubspot_crm/bigquery/stitch/stg_hubspot_crm_pipelines.sql
+++ models/sources/stg_hubspot_crm/bigquery/stitch/stg_hubspot_crm_pipelines.sql
@@ -1,22 +1,33 @@
-{% if target.type == 'bigquery' %}
+{% if target.type == "bigquery" %}
 {% if var("marketing_warehouse_deal_sources") %}
-{% if 'hubspot_crm' in var("marketing_warehouse_deal_sources") %}
-{% if var("stg_hubspot_crm_etl") == 'stitch' %}
+{% if "hubspot_crm" in var("marketing_warehouse_deal_sources") %}
+{% if var("stg_hubspot_crm_etl") == "stitch" %}
 
-with source as (
-  {{ filter_stitch_relation(relation=source('stitch_hubspot_crm','pipelines'),unique_column='pipelineid') }}
-),
-renamed as (
-    select
-      label as pipeline_label,
-      pipelineid as pipeline_id,
-      displayorder as pipeline_display_order,
-      active as pipeline_active
-    from source
-)
-select * from renamed
+with
+    source as (
+        {{
+            filter_stitch_relation(
+                relation=source("stitch_hubspot_crm", "pipelines"),
+                unique_column="pipelineid",
+            )
+        }}
+    ),
+    renamed as (
+        select
+            label as pipeline_label,
+            pipelineid as pipeline_id,
+            displayorder as pipeline_display_order,
+            active as pipeline_active
+        from source
+    )
+select *
+from renamed
 
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
-{% else %} {{config(enabled=false)}} {% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
+{% else %} {{ config(enabled=false) }}
+{% endif %}
