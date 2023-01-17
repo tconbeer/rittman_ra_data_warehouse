dc5e00fc067ec916e8df4b2ed17589be7e4ccd8c
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-01-17 22:35:06 +0000

    chore: apply sqlfmt e6f9f4c

--- models/integration/int_subscription_billing.sql
+++ models/integration/int_subscription_billing.sql
@@ -1,20 +1,12 @@
-{% if var("subscriptions_warehouse_sources")  %}
+{% if var("subscriptions_warehouse_sources") %}
 
-
-with subscription_invoices_merge_list as
-  (
-    SELECT *
-    FROM   {{ ref('stg_stripe_subscriptions_invoices') }}
-  )
-select * from subscription_invoices_merge_list
-
-{% else %}
-
-{{
-    config(
-        enabled=false
+with
+    subscription_invoices_merge_list as (
+        select * from {{ ref("stg_stripe_subscriptions_invoices") }}
     )
-}}
+select *
+from subscription_invoices_merge_list
 
+{% else %} {{ config(enabled=false) }}
 
 {% endif %}
